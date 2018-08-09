c59d0d50b3421eaef6a106e8e1ba1a2110e1e6c7
otyn0308 <elements0308@gmail.com>
2018-08-10 02:15:17 +0900

    四角形を描いた

--- bootpack.c
+++ bootpack.c
@@ -6,17 +6,35 @@ void io_store_eflags(int eflags);
 
 void init_palette(void);
 void set_palette(int start, int end, unsigned char *rgb);
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
+
+#define COL8_000000		0
+#define COL8_FF0000		1
+#define COL8_00FF00		2
+#define COL8_FFFF00		3
+#define COL8_0000FF		4
+#define COL8_FF00FF		5
+#define COL8_00FFFF		6
+#define COL8_FFFFFF		7
+#define COL8_C6C6C6		8
+#define COL8_840000		9
+#define COL8_008400		10
+#define COL8_848400		11
+#define COL8_000084		12
+#define COL8_840084		13
+#define COL8_008484		14
+#define COL8_848484		15
 
 void HariMain(void){
-  int i;
   char *p;
 
   init_palette();
+
   p = (char *) 0xa0000;
 
-  for(i = 0; i <= 0xffff; i++){
-    p[i] = i & 0x0f;
-  }
+  boxfill8(p, 320, COL8_FF0000, 20, 20, 120, 120);
+  boxfill8(p, 320, COL8_00FF00, 70, 50, 170, 150);
+  boxfill8(p, 320, COL8_0000FF, 120, 80, 220, 180);
 
   for(;;){
     io_hlt();
@@ -58,5 +76,14 @@ void set_palette(int start, int end, unsigned char *rgb){
     rgb += 3;
   }
   io_store_eflags(eflags);
+  return;
+}
+
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1){
+  int x, y;
+  for(y = y0; y <= y1; y++){
+    for(x = x0; x <= x1; x++)
+      vram[y * xsize + x] = c;
+  }
   return;
 }
