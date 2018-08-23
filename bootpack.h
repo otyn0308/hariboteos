ef12a48d4499c95d80e3f7077ebc1d32ba33ddd7
otyn0308 <elements0308@gmail.com>
2018-08-24 01:04:32 +0900

    know where pressed keybord

--- bootpack.h
+++ bootpack.h
@@ -14,6 +14,7 @@ struct BOOTINFO{
 void io_hlt(void);
 void io_cli(void);
 void io_sti(void);
+int io_in8(int port);
 void io_out8(int port, int data);
 int io_load_eflags(void);
 void io_store_eflags(int eflags);
