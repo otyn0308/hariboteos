8f02e0ccdc8ef748a21f1c0110588e68fcd0e5b5
otyn0308 <elements0308@gmail.com>
2018-08-26 02:47:17 +0900

    something changed

--- bootpack.c
+++ bootpack.c
@@ -9,6 +9,7 @@ void HariMain(void){
   struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
   char s[40], mcursor[256], keybuf[32], mousebuf[128];
   int mx, my, i;
+  unsigned char mouse_dbuf[3],mouse_phase;
 
   init_gdtidt();
   init_pic();
@@ -31,6 +32,7 @@ void HariMain(void){
   putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
   enable_mouse();
+  mouse_phase = 0;
 
   for(;;){
     io_cli();
@@ -46,9 +48,23 @@ void HariMain(void){
       }else if(fifo8_status(&mousefifo) != 0){
         i = fifo8_get(&mousefifo);
         io_sti();
-        sprintf(s, "%02X", i);
-        boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 47, 31);
-        putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+        if(mouse_phase == 0){
+          if(i == 0xfa){
+            mouse_phase = 1;
+          }
+        }else if(mouse_phase == 1){
+          mouse_dbuf[0] = i;
+          mouse_phase = 2;
+        }else if(mouse_phase == 2){ 
+          mouse_dbuf[1] = i;
+          mouse_phase = 3;
+        }else if(mouse_phase == 3){
+          mouse_dbuf[2] = i;
+          mouse_phase = 1;
+          sprintf(s, "%02X %02X %02X", mouse_dbuf[0],  mouse_dbuf[1], mouse_dbuf[2]);
+          boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 32, 16, 47, 31);
+          putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL8_FFFFFF, s);
+        }
       }
     }
   }
