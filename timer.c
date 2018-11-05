288b7934c46a0b36c0de180f7b2c04e48f72b873
otyn0308 <elements0308@gmail.com>
2018-11-06 05:52:08 +0900

    13-6

--- timer.c
+++ timer.c
@@ -10,6 +10,7 @@ struct TIMERCTL timerctl;
 
 void init_pit(void){
   int i;
+  struct TIMER *t;
   io_out8(PIT_CTRL, 0x34);
   io_out8(PIT_CNT0, 0x9c);
   io_out8(PIT_CNT0, 0x2e);
@@ -19,6 +20,12 @@ void init_pit(void){
   for(i = 0; i < MAX_TIMER; i++){
     timerctl.timers0[i].flags = 0;
   }
+  t = timer_alloc();
+  t->timeout = 0xffffffff;
+  t->flags = TIMER_FLAGS_USING;
+  t->next = 0;
+  timerctl.t0 = t;
+  timerctl.next = 0xffffffff;
   return;
 }
 
@@ -52,13 +59,6 @@ void timer_settime(struct TIMER *timer, unsigned int timeout){
   e = io_load_eflags();
   io_cli();
   timerctl.using++;
-  if(timerctl.using == 1){
-    timerctl.t0 = timer;
-    timer->next = 0;
-    timerctl.next = timer->timeout;
-    io_store_eflags(e);
-    return;
-  }
   t = timerctl.t0;
   if(timer->timeout <= t->timeout){
     timerctl.t0 = timer;
@@ -70,9 +70,6 @@ void timer_settime(struct TIMER *timer, unsigned int timeout){
   for(;;){
     s = t;
     t = t->next;
-    if(t == 0){
-      break;
-    }
     if(timer->timeout <= t->timeout){
       s->next = timer;
       timer->next = t;
@@ -80,14 +77,9 @@ void timer_settime(struct TIMER *timer, unsigned int timeout){
       return;
     }
   }
-  s->next = timer;
-  timer->next = 0;
-  io_store_eflags(e);
-  return;
 }
 
 void inthandler20(int *esp){
-  int i;
   struct TIMER *timer;
   io_out8(PIC0_OCW2, 0x60);
   timerctl.count++;
@@ -95,7 +87,7 @@ void inthandler20(int *esp){
     return;
   }
   timer = timerctl.t0;
-  for(i = 0; i < timerctl.using; i++){
+  for(;;){
     if(timer->timeout > timerctl.count){
       break;
     }
@@ -103,12 +95,7 @@ void inthandler20(int *esp){
     fifo32_put(timer->fifo, timer->data);
     timer = timer->next;
   }
-  timerctl.using -= i;
   timerctl.t0 = timer;
-  if(timerctl.using > 0){
-    timerctl.next = timerctl.t0->timeout;
-  }else{
-    timerctl.next = 0xffffffff;
-  }
+  timerctl.next = timer->timeout;
   return;
 }
