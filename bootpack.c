a33f03cdcc9933e39234306f86c14d37f118b23b
otyn0308 <elements0308@gmail.com>
2018-11-12 10:25:59 +0900

    character input

--- bootpack.c
+++ bootpack.c
@@ -17,6 +17,14 @@ void HariMain(void){
   struct SHTCTL *shtctl;
   struct SHEET *sht_back, *sht_mouse, *sht_win;
   unsigned char *buf_back, buf_mouse[256], *buf_win;
+  static char keytable[0x54] = {
+    0,   0,   '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '^', 0,   0,
+    'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '@', '[', 0,   0,   'A', 'S',
+    'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', ':', 0,   0,   ']', 'Z', 'X', 'C', 'V',
+    'B', 'N', 'M', ',', '.', '/', 0,   '*', 0,   ' ', 0,   0,   0,   0,   0,   0,
+    0,   0,   0,   0,   0,   0,   0,   '7', '8', '9', '-', '4', '5', '6', '+', '1',
+    '2', '3', '0', '.'
+  };
 
   init_gdtidt();
   init_pic();
@@ -55,7 +63,7 @@ void HariMain(void){
   sheet_setbuf(sht_win, buf_win, 160, 52, -1);
   init_screen8(buf_back, binfo->scrnx, binfo->scrny);
   init_mouse_cursor8(buf_mouse, 99);
-  make_window8(buf_win, 160, 52, "counter");
+  make_window8(buf_win, 160, 52, "window");
   sheet_slide(sht_back, 0, 0);
   mx = (binfo->scrnx - 16) / 2;
   my = (binfo->scrny - 28 - 16) / 2;
@@ -79,8 +87,12 @@ void HariMain(void){
       if(256 <= i && i <= 511){
         sprintf(s, "%02X", i - 256);
         putfonts8_asc_sht(sht_back, 0, 16, COL8_FFFFFF, COL8_008484, s, 2);
-        if(i == 0x1e + 256){
-          putfonts8_asc_sht(sht_win, 40, 28, COL8_000000, COL8_C6C6C6, "A", 1);
+        if(i < 256 + 0x54){
+          if(keytable[i - 256] != 0){
+            s[0] = keytable[i - 256];
+            s[1] = 0;
+            putfonts8_asc_sht(sht_win, 40, 28, COL8_000000, COL8_C6C6C6, s, 1);
+          }
         }
       }else if(512 <= i && i <= 767){
         if(mouse_decode(&mdec, i - 512) != 0){
